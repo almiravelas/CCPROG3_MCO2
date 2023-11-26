5a606a71c90c5bd64e48d648a1382f5162b6764a
Almira Velasquez <144332594+almiravelas@users.noreply.github.com>
2023-11-26 15:35:35 +0800

    Delete GAME/Tools.java

--- GAME/Tools.java
+++
@@ -1,9 +0,0 @@
-public class Tools {
-    public static void Clear() {
-        System.out.println("--------------");
-    }
-
-    public static void Pause() {
-        System.out.print("Press enter to continue...");
-    }
-}
